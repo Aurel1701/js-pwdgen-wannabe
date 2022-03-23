de1d85c58c283bb1e28d6c3c45f53bc9a611ef27
Aurel1701 <gantzfan96@gmail.com>
2022-03-23 15:03:30 +0100

    edit

---
+++ assets/main.js
@@ -0,0 +1,19 @@
+// chiedi il nome all'utente
+const firstname = prompt('Inserisci il tuo nome')
+
+console.log(firstname);
+
+
+// chiedi il cognome all'utente
+const lastname = prompt('Inserisci il tuo cognome')
+
+console.log(lastname);
+
+// chiedi il suo colore preferito all'utente
+const favoritecolor = prompt('Inserisci il tuo colore preferito')
+
+console.log(favoritecolor);
+
+
+
+document.getElementById(utente).innerHTML = favoritecolor
